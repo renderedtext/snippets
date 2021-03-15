a0585fa15d2700b133bd7cf9d92948fefde12a8a
Marko Bogdanović <mbogdanovic@renderedtext.com>
2021-03-15 13:30:43 +0100

    Update install_docker_debug.sh

--- install_docker_debug.sh
+++ install_docker_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/renderedtext/snippets/master/push.sh > /tmp/docker-debug
-chmod +x /tmp/docker-debug
-sudo mv /tmp/docker-debug /usr/local/bin
+curl https://raw.githubusercontent.com/renderedtext/snippets/master/push.sh > /tmp/docker-debug-push
+chmod +x /tmp/docker-debug-push
+sudo mv /tmp/docker-debug-push /usr/local/bin
