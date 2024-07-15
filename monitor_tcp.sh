874cdef3bf67cb9196c0842a6ac82bdecacc072d
Nandor Bors <nbors@renderedtext.com>
2024-07-15 12:22:32 +0300

    update tcp monitor

--- monitor_tcp.sh
+++ monitor_tcp.sh
@@ -2,7 +2,20 @@
 HOST=${1:-'1'}
 PORT=${2:-'0'}
 
-#sudo apt-get install -y mtr
+DIST=$(uname)
+
+  case $DIST in
+    Linux)
+      which mtr || (sudo apt-get update && sudo apt-get install -y mtr)
+      ;;
+    Darwin)
+      which mtr || (brew install mtr)
+      ;;
+    *)
+      echo "Unsupported distro $DIST"
+      exit 1
+      ;;
+  esac
 
 if [[ $# -ne 2 ]];then
   echo "Usage: ./monitor_tcp.sh [hostname|host ip] [tcp port number]"
@@ -17,4 +30,4 @@ if [[ $PORT == '0' ]];then
   exit 1
 fi
 
-while true; do sudo mtr -P ${PORT} -T ${HOST} -c 10 -r -n >> /tmp/mtr.log;sleep 1; done
+while true; do sudo mtr -P ${PORT} -T ${HOST} -c 10 -i 0.5 -G 0.1 -r -n -o "LDAWS" >> /tmp/mtr.log;sleep 0.5; done
