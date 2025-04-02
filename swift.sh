d1c66741e71cee63b0be1ed163990a7d56b784a5
Nandor Bors <nbors@renderedtext.com>
2025-04-02 11:35:57 +0300

    Add swift version as paramether to swift.sh

--- swift.sh
+++ swift.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#
+#  Version can be selected by 'source swift.sh [5.10|6.1]' or '. swift.sh [5.10|6.1]'
+#  The default version is 5.10. If version is missing, default is used
+#
+
+swiftVersion="${1:-'5.10'}"
 SWVersion=$(lsb_release -rs)
 
-wget http://packages.semaphoreci.com/swift/swift-5.10-RELEASE-ubuntu${SWVersion}.tar.gz -O /tmp/swift-5.10-RELEASE-ubuntu${SWVersion}.tar.gz
+wget http://packages.semaphoreci.com/swift/swift-${swiftVersion}-RELEASE-ubuntu${SWVersion}.tar.gz -O /tmp/swift-${swiftVersion}-RELEASE-ubuntu${SWVersion}.tar.gz
 cd /tmp/
-tar -zxf swift-5.10-RELEASE-ubuntu${SWVersion}.tar.gz
-sudo mv /tmp/swift-5.10-RELEASE-ubuntu${SWVersion} /opt/swift
+tar -zxf swift-${swiftVersion}-RELEASE-ubuntu${SWVersion}.tar.gz
+sudo mv /tmp/swift-${swiftVersion}-RELEASE-ubuntu${SWVersion} /opt/swift
 echo "export PATH=/opt/swift/usr/bin:$PATH" >> ~/.bashrc 
 source ~/.bashrc
 swift --version
